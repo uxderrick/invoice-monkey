e7f31db4be5513378bbdc9b7fe1c6ff5fd2c4c94
UXDerrick <tsormed@gmail.com>
2024-02-13 13:14:22 +0000

    Update Firebase API key and add image assets

--- src/components/Confirmation.jsx
+++ src/components/Confirmation.jsx
@@ -1,3 +1,6 @@
+/* eslint-disable no-empty-pattern */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
 import React from "react";
 import {
   Heading,
@@ -10,6 +13,7 @@ import {
   Popover,
   TextField,
 } from "@radix-ui/themes";
+import Confirm from "../assets/confirm.png";
 
 const Confirmation = ({}) => {
   const handleNextClick = () => {
@@ -23,11 +27,7 @@ const Confirmation = ({}) => {
     <>
       {/* //////////////////////////////////////////////// */}
       {/* Question 5 */}
-      <img
-        src="https://s3-alpha-sig.figma.com/img/2dd1/3068/39b34bcafd060686d1a1aa4b1e673653?Expires=1701648000&Signature=p7yi-2BdKJs4Lgig2mfExCmoJo-qkXkfz0Bjr5L0uykGnH8RGbpvCtXoQxiyf7jzGq7ptssfiKsQMNWHX0j8khk-fqsjmMMSaCE33-3rLmm1IussOz9d54kCVrRu7dHMJz9EZ8cG~s-RkvhVOs-cvye5q6MzLk5saxR2uM1EMfrp1DIuSRn49ZdwVRutc1exNNLhC~kQ2O0oUFup19PJA9zgETENAc6hBLdKR4JML4wO5sT5p7KJiPxfKYg8VsmO86b38-rmAX7Evn4KjR7NvrEAGj1c6~f8BI9jPoQxtjihHeSF~htR-3EoK1UkzZu3GzfcuQxecAs6aGiyo76avg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
-        height={80}
-        width={80}
-      ></img>
+      <img src={Confirm} height={80} width={80}></img>
       <Flex
         direction="column"
         gap={{
--- src/components/CreateInvoice.jsx
+++ src/components/CreateInvoice.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
 import {
   Heading,
@@ -8,6 +9,7 @@ import {
   Avatar,
   Separator,
 } from "@radix-ui/themes";
+import Invoice from "../assets/invoice.png";
 
 const CreateInvoice = () => {
   return (
@@ -21,10 +23,7 @@ const CreateInvoice = () => {
         window.location.href = "/new";
       }}
     >
-      <img
-        src="https://s3-alpha-sig.figma.com/img/21ba/36b9/3717f5840dc5afcadc69b95b90d1eff6?Expires=1701648000&Signature=Sx-jzkAF-bsMUUxOYEFPOVPtLa2X5GzOcxTH8iTcnKvcDbwNCKBcaYZR6XzZZwz0qX5gYjv4k2uzcCEe--Pw1awH2TTG0E923MdC-zrzPtxBc~cNRG2imsjPUDkulSncitdxIV74tIdnSzwWA4eX0z97ugStP22ocDF797cNd459RR7nxrWx0bIMd1xgXH~emRMqN2lOKh~06xzVjbPlU7-PGfu56AOFcJn9jZKOvTWmHc2B5EUwG1NzUFZxSfhAt9gyF4r5C6TMS1T~jzNUXa44f-GGH15PaL8HOaiabSF8BCy-syFXlBEkBXmbbU6wyLz4MFtDvZ6HjHnxL42OvA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
-        height={56}
-      ></img>
+      <img src={Invoice} height={56}></img>
       <Text
         align="left"
         style={{
